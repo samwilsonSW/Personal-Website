e9c273407c2cd5b2f0a11c6373bde54b701acb0e
Sam Wilson <sam.wilson@ttu.edu>
2023-04-12 15:04:18 -0500

    Add Dolce Vita font

--- pages/_document.js
+++ pages/_document.js
@@ -3,7 +3,14 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head> <link
+          rel="preload"
+          href="/fonts/Dolce Vita.ttf"
+          as="font"
+          type="font/ttf"
+          crossOrigin="anonymous"
+        />
+      </Head>
       <body>
         <Main />
         <NextScript />
