babb0cf84c8a98471031547bbe86e3a8bfc0d721
Sam Wilson <sam.wilson@ttu.edu>
2023-04-12 16:21:20 -0500

    Add hrefs

--- pages/_document.js
+++ pages/_document.js
@@ -3,9 +3,31 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head> <link
+      <Head> 
+        <link
           rel="preload"
           href="/fonts/Dolce Vita.ttf"
+          as="font"
+          type="font/ttf"
+          crossOrigin="anonymous"
+        />
+        <link
+          rel="preload"
+          href="/fonts/Dolce Vita Light.ttf"
+          as="font"
+          type="font/ttf"
+          crossOrigin="anonymous"
+        />
+        <link
+          rel="preload"
+          href="/fonts/Dolce Vita Heavy.ttf"
+          as="font"
+          type="font/ttf"
+          crossOrigin="anonymous"
+        />
+        <link
+          rel="preload"
+          href="/fonts/Century Gothic.ttf"
           as="font"
           type="font/ttf"
           crossOrigin="anonymous"
